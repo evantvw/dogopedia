36eda8ec2180ebad9e149e92472f97153c801286
Evant Valery <valeryevantq@gmail.com>
2026-02-06 22:36:29 +0700

    chore: add image remotePatterns in next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL("https://cdn4.thedogapi.com/**")],
+  },
 };
-
 export default nextConfig;
