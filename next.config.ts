784c9ccbe08ad71fdf2faab456e8f3c8f0437215
Evant Valery <valeryevantq@gmail.com>
2026-02-08 23:56:00 +0700

    chore: configure config file to allow new image src domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://cdn4.thedogapi.com/**")],
+    remotePatterns: [
+      new URL("https://cdn4.thedogapi.com/**"),
+      new URL("https://storage.googleapis.com/**"),
+    ],
   },
 };
 export default nextConfig;
