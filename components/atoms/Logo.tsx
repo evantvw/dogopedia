096acbfc8e0dcee451218b5b14c7c2fbf4aba704
Evant Valery <valeryevantq@gmail.com>
2026-02-17 13:22:39 +0700

    fix: fix broken image in Logo component

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -2,7 +2,12 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <Image alt="dogopedia-logo" src={"dogopedia.svg"} height={36} width={178} />
+    <Image
+      alt="dogopedia-logo"
+      src={"/dogopedia.svg"}
+      height={36}
+      width={178}
+    />
   );
 };
 
