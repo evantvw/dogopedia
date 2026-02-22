cf68bbf38ca28e05df86f68f03857aca6d301b28
Evant Valery <valeryevantq@gmail.com>
2026-02-22 10:06:57 +0700

    feat: create custom fallback content when image is broken and improve UX by changing font

--- components/organisms/Card.tsx
+++ components/organisms/Card.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
   id: number;
@@ -12,28 +15,38 @@ interface Props {
 }
 
 const Card = ({ id, src, alt, title, traits, height, weight }: Props) => {
+  const [imageError, setImageError] = useState(false);
   return (
     <div className="card bg-base-100 shadow-sm col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 overflow-hidden cursor-pointer">
       <Link href={`/breeds/${id}`}>
         {/* Image Container: Added 'relative' and 'h-48' (or aspect-square) */}
         <div className="relative h-48 w-full">
-          <Image
-            src={src}
-            alt={alt}
-            fill
-            className="object-cover object-[50%_15%]"
-            sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-          />
+          {!imageError ? (
+            <Image
+              src={src}
+              alt={alt}
+              fill
+              className="object-cover object-[50%_15%]"
+              sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+              onError={() => {
+                setImageError(true);
+              }}
+            />
+          ) : (
+            <div className="bg-slate-200 text-gray-500 text-sm font-medium size-full flex flex-col justify-center items-center">
+              No Image Available
+            </div>
+          )}
         </div>
 
         <div className="card-body p-5 gap-2.5">
-          <h2 className="card-title font-bold text-xl">{title}</h2>
+          <h2 className="card-title font-bold text-xl mb-2">{title}</h2>
 
           {/* Traits Tags */}
           <div className="flex gap-2.5 flex-wrap">
             {traits.map((item) => (
               <div className="py-1 px-2 rounded-md bg-[#E7FDE7]" key={item}>
-                <p className="text-xs font-medium text-[#053005]">{item}</p>
+                <p className="text-xs font-medium text-[#053005] font-inter lowercase">{item}</p>
               </div>
             ))}
           </div>
@@ -42,7 +55,7 @@ const Card = ({ id, src, alt, title, traits, height, weight }: Props) => {
           <div className="flex gap-2.5 items-center mt-auto">
             <div className="flex gap-1.5 items-center">
               <Image src="/paw.svg" height={14} width={14} alt="Height icon" />
-              <p className="text-xs text-[#618961]">
+              <p className="text-xs text-[#618961] font-inter">
                 {height.min}-{height.max}cm
               </p>
             </div>
@@ -56,7 +69,7 @@ const Card = ({ id, src, alt, title, traits, height, weight }: Props) => {
                 width={14}
                 alt="Weight icon"
               />
-              <p className="text-xs text-[#618961]">
+              <p className="text-xs text-[#618961] font-inter">
                 {weight.min}-{weight.max}kg
               </p>
             </div>
