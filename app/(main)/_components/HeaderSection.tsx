424904338b9264530184dc2a529cb73b850926dc
Evant Valery <valeryevantq@gmail.com>
2026-02-22 10:06:21 +0700

    feat: create overlay for better font contrast

--- app/(main)/_components/HeaderSection.tsx
+++ app/(main)/_components/HeaderSection.tsx
@@ -2,12 +2,13 @@ import SearchBar from "@/components/molecules/SearchBar";
 
 const HeaderSection = () => {
   return (
-    <section className="hero bg-[url(/hero.png)] min-h-140 rounded-2xl mt-5 flex flex-col gap-8 bg-position-[50%_0%]">
-      <h1 className="font-extrabold text-5xl md:text-7xl text-white text-center mt-32">
+    <section className="hero bg-[url(/hero.png)] min-h-140 rounded-2xl mt-5 flex flex-col gap-8 bg-position-[50%_0%] relative">
+      <div className="absolute inset-0 bg-black opacity-35 rounded-2xl" />
+      <h1 className="font-extrabold text-5xl md:text-7xl text-white text-center mt-32 z-10">
         Find your perfect <br />
         companion.
       </h1>
-      <h2 className="font-light text-lg md:text-2xl text-neutral-100 text-center font-inter">
+      <h2 className="font-light text-lg md:text-2xl text-neutral-100 text-center font-inter z-10">
         The ultimate encyclopedia for dog lovers. Discover breeds, facts,
         <br /> and more.
       </h2>
