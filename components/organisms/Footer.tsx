a6d9d298249907a6a5f1570d9aa494af9fcac103
Evant Valery <valeryevantq@gmail.com>
2026-02-22 10:13:28 +0700

    refactor: fix component's logic and move the constant variable outside functional component

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,8 +1,23 @@
-import { Copyright } from "lucide-react";
+"use client";
+
 import Logo from "../atoms/Logo";
 import NavLink from "../atoms/NavLink";
 
+import { Copyright } from "lucide-react";
+import { cn } from "@/libs/utils";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    label: "Breeds",
+    path: "/breeds",
+  },
+  { label: "About", path: "/about" },
+  { label: "Github", path: "https://github.com/evantvw", target: "_blank" },
+];
+
 const Footer = () => {
+  const pathName = usePathname();
   return (
     <footer className="flex flex-col md:flex-row justify-between items-start md:items-center gap-9 md:gap-4 bg-base-100 px-5 py-6">
       <section>
@@ -12,11 +27,21 @@ const Footer = () => {
       </section>
 
       <section className="flex flex-col gap-1 md:flex-row md:gap-10 md:self-center">
-        <NavLink href="/breeds">Breeds</NavLink>
-        <NavLink href="/about">About</NavLink>
-        <NavLink href="https://github.com/evantvw" target="_blank">
-          Github
-        </NavLink>
+        {links.map((item) => {
+          const isActive = pathName.startsWith(item.path);
+          return (
+            <NavLink
+              key={item.path}
+              href={item.path}
+              target={item.target}
+              className={cn("hover:text-[#0FAE0F]", {
+                "text-[#0FAE0F] font-bold": isActive,
+              })}
+            >
+              {item.label}
+            </NavLink>
+          );
+        })}
       </section>
 
       <section className="flex gap-2 items-center">
--- components/organisms/Navbar.tsx
+++ components/organisms/Navbar.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { cn } from "@/libs/utils";
-import { usePathname } from "next/navigation";
-
 import Logo from "../atoms/Logo";
 import NavLink from "../atoms/NavLink";
 
-const Navbar = () => {
-  const links = [
-    {
-      label: "Breeds",
-      path: "/breeds",
-    },
-    { label: "About", path: "/about" },
-    { label: "Github", path: "https://github.com/evantvw", target: "_blank" },
-  ];
-  const pathName = usePathname();
+import { cn } from "@/libs/utils";
+import { usePathname } from "next/navigation";
 
+const links = [
+  {
+    label: "Breeds",
+    path: "/breeds",
+  },
+  { label: "About", path: "/about" },
+  { label: "Github", path: "https://github.com/evantvw", target: "_blank" },
+];
+
+const Navbar = () => {
+  const pathName = usePathname();
   return (
     <nav className="flex justify-between items-center bg-base-100 py-3 px-5 sticky top-0 z-50 shadow-lg">
       <NavLink href="/">
@@ -25,13 +25,13 @@ const Navbar = () => {
 
       <section className="hidden sm:flex gap-10">
         {links.map((item) => {
-          const isActive = item.path === pathName;
+          const isActive = pathName.startsWith(item.path);
           return (
             <NavLink
               key={item.path}
               href={item.path}
               target={item.target}
-              className={cn("hover:text-[#0FAE0F]",{
+              className={cn("hover:text-[#0FAE0F]", {
                 "text-[#0FAE0F] font-bold": isActive,
               })}
             >
