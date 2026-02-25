bf70f2c7e2d0594c574b67244b0f7e41dadfc5c0
Evant Valery <valeryevantq@gmail.com>
2026-02-25 22:54:32 +0700

    feat: enable user to filter searches

--- app/(main)/breeds/_components/FilterSection.tsx
+++ app/(main)/breeds/_components/FilterSection.tsx
@@ -1,12 +1,34 @@
+"use client";
+
 import SearchBar from "@/components/molecules/SearchBar";
 import SelectInput from "@/components/molecules/SelectInput";
+
+import { sortOptions } from "@/constants";
 import { IBreed } from "@/types/DogList";
+import { useSearchParams, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
 
 interface Props {
   breedList: IBreed[];
 }
 
 const FilterSection = ({ breedList }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("search") ?? "";
+  const sortQuery = searchParams.get("sort") ?? "ASC";
+
+  const [sort, setSort] = useState(sortQuery);
+
+  const handleSort = (e: ChangeEvent<HTMLSelectElement>) => {
+    const newValue = e.target.value;
+    setSort(newValue);
+    if (searchQuery.trim() !== "") {
+      router.push(`/breeds?search=${searchQuery}&sort=${newValue}`);
+    } else {
+      router.push(`/breeds?sort=${newValue}`);
+    }
+  };
   return (
     <section className="rounded-2xl shadow-sm px-5 py-3 mt-10 bg-white flex flex-col lg:flex-row lg:justify-between lg:items-center gap-3">
       {/* searchbar and limit option */}
@@ -19,7 +41,12 @@ const FilterSection = ({ breedList }: Props) => {
         <p className="font-inter font-medium text-gray-800 text-base">
           {breedList.length} breeds
         </p>
-        <SelectInput options={[{ label: "ASC" }]} className="w-20 border-neutral-200" />
+        <SelectInput
+          options={sortOptions}
+          className="w-20 border-neutral-200"
+          value={sort}
+          onChange={handleSort}
+        />
       </div>
     </section>
   );
